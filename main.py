e03dbe381531cfa7207716d025370bb4100d4723
Vitas <twandy543hient@gmail.com>
2024-07-03 16:57:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -63,14 +63,14 @@ def linebot(request):
 
 def messageToSend(userId):
     # design a function to send message, return string
-    userDonateInformation = getUserDonateData(userId)['DonateInformation']
+    userDonateInformation = getUserDonateData(userId)
     # only send the message to the person who really donate to the church
-    if userDonateInformation[-1] != '0':
-        content = f'''Hello {userDonateInformation[0]}, 收到你的奉獻如下: 
-一般奉獻: {userDonateInformation[1]}元, 
-十一奉獻: {userDonateInformation[2]}元, 
-ARK奉獻: {userDonateInformation[3]}元, 
-總奉獻: {userDonateInformation[4]}元
+    if userDonateInformation['總奉獻'] != '0':
+        content = f'''Hello {userDonateInformation['名字']}, 收到你的奉獻如下: 
+一般奉獻: {userDonateInformation['一般奉獻']}元, 
+十一奉獻: {userDonateInformation['十一奉獻']}元, 
+ARK奉獻: {userDonateInformation['ARK奉獻']}元, 
+總奉獻: {userDonateInformation['總奉獻']}元
 謝謝你慷慨的給予！'''
         return content
     return ''
